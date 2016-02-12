eed23b6b0b5b0c5c91e1be7ea8db1fb130887fc0
nguc <c.ngu.91@gmail.com>
2016-02-12 00:28:42 -0700

    Completed Point and Test classes. Finished javadoc

--- Point/Point.java
+++ Point/Point.java
@@ -1,26 +1,43 @@
-//get rid of redundant code by calling mehtods or using this with parameters inside for constructors.
-public class Point{
+/**
+Creates point objects with defalut coordinates or coordinates that are passed in by the user, each object created will have its own unique ID number. Point objects can be moved by values entered in as deltaX and deltaY using the move method. The distance between two points can be calculated using the distance method. An object's coordinates can be set to be equal to the average of 3 other point objects. The number of active objects will also be tracked
 
+@author Chi Nguyen
+*/
+public class Point{
+	//Variables
 	private int xCoord, yCoord;
+	public int avgX,avgY;
 	public double distance;
 	public int ID;
 	private static int numOfPoints = 0;
 	public static int activeIns=0;
 
-	///Constructors
+	/**
+	Creates a point at 0,0 and assigns each point an ID number and increments the number of active instansces
+	*/
 	public Point()
 	{
-		this(0,0);
-	}
-
-	public Point(int x, int y)
-	{
-		this.setXAndY(x,y);
+		this.setXAndY(0,0);
 		activeIns();
 		ID = ++numOfPoints;
 	}
+	/**
+	Creates a point with the entered values
 
-	//Set methods
+	@param x the value entered for the x coordinate of the point
+	@param y the value entered for the y coordinate of the point
+	*/
+	public Point(int x, int y)
+	{
+		this();
+		this.setXAndY(x,y);
+	}
+	/**
+	Registers the values entered by the user to the xCoord and yCoord. X and y coordinate values can be registerd at the same time or individually
+
+	@param x value of x entered to be set to xCoord
+	@param y value of y entered to be set to yCoord
+	*/
 	public void setXAndY(int x, int y)
 	{
 		xCoord = x;
@@ -37,55 +54,105 @@ public class Point{
 		yCoord = y;
 	}
 
-	//Get methods
-	// use /**   */ for javadoc comment
-	public int getX()
+	/**
+	Returns the value of the x or y coordinate
+
+	@return xCoord gives value of x
+	@return yCoord gives valeue of y
+	*/
+	public double getX()
 	{
 		return xCoord;
 	}
 
-	public int getY()
+	public double getY()
 	{
 		return yCoord;
 	}
 
-	//Shifts point according to the deltaX and deltaY values
+	/**
+	Shifts point according to the deltaX and deltaY values
+
+	@param deltaX value that the x coordinate of the point will move by
+	@param deltaY value that the y coordinate of the pont will move by
+	*/
 	public void move(int deltaX, int deltaY)
 	{
 		xCoord = xCoord + deltaX;
 		yCoord = yCoord + deltaY;
 	}
 
-	//Calculates the distance between point object and a point passed in as a parameter
+	/**
+	Calculates the distance between point object and a point passed in as a parameter
+
+	@param point the point that the distance is measured to
+	@return distance The calculated distance between the object and the point that is passed in
+	*/
 	public double distance(Point point)
 	{
 		distance = Math.sqrt(Math.pow((point.xCoord - this.xCoord),2) + Math.pow((point.yCoord - this.yCoord),2)); 
 		return distance;
 	}
-	// static method of distance
+
+	/** 
+	Two points are passed in as parameters and the distance between them is calculated
+
+	@param p1 the first point object that is passed in
+	@param p2 the second point object that is passed in to calculate the distance between it and the first point
+	@return distance calculated between the first point and the second point that were passed in
+	*/
 	public static double distance(Point p1,Point p2)
 	{
 		return p1.distance(p2);
 	}
 
-	//Gives each object an ID number
+	/**
+	Calculates the average x and y coordinates of the first three points and onverts the average values to integers.Then sets the x and y coordinates of the object to those values
+
+	@param p1 first point object that is passed in
+	@param p2 second point object that is passed in
+	@param p3 third point object that is passed in
+	*/
+	public void average(Point p1,Point p2,Point p3)
+	{
+		avgX = (p1.xCoord + p2.xCoord + p3.xCoord)/3;
+		avgY = (p1.yCoord + p2.yCoord + p3.yCoord)/3;
+		this.setXAndY((int)avgX,(int)avgY);
+	}
+
+	/**
+	Assigns each object an ID number
+
+	@return returns an ID number to the object
+	*/
 	public int pointID()
 	{
 		return ID;
 	}
 
-	//Keeps track of the number of points that are active
+	/**
+	Keeps track of the number of active point objects, increments after every call
+
+	@return number of active points
+	*/
 	public int activeIns()
 	{
 		return activeIns++;
 	}
 
+	/**
+	Allows the point objects to be displayed as a string
+
+	@return object information as a string in the form: (x,y)
+	*/
 	public String toString()
 	{
 		return "(" + this.xCoord + "," + this.yCoord +")";
-		
 	}
 
+	/**
+	Garbage collection, deletes objects that are no longer referenced to from memeory and the number of active instances will be decremented for each object that is deleted
+	*/
 	protected void finalize() throws Throwable
 	{
 		super.finalize();
--- Point/Test.java
+++ Point/Test.java
@@ -8,51 +8,63 @@ class Test{
 		//Dont forget to close scanner at the end of the program!!!!!!!!
 		Scanner userInput = new Scanner(System.in);
 		int xCoord, yCoord;
-
+		//prompt user for coordinates of first point
 		System.out.println("Enter an x value for point 1: ");
 		xCoord = userInput.nextInt();
 		System.out.println("Enter a y value for point 1: ");
 		yCoord = userInput.nextInt();
 		Point point1 = new Point(xCoord,yCoord);
-		System.out.println("Current number of points: " + Point.activeIns);
-
-		/*System.out.println("Enter an x value for point 2: ");
+		//prompt user for coordinates of second point
+		System.out.println("Enter an x value for point 2: ");
 		xCoord = userInput.nextInt();
 		System.out.println("Enter a y value for point 2: ");
-		yCoord = userInput.nextInt();*/
-		Point point2 = new Point();
-		System.out.println("Current number of points: " + Point.activeIns);
-
-/*
+		yCoord = userInput.nextInt();
+		Point point2 = new Point(xCoord,yCoord);
+		//prompt user for coordinates of third point
 		System.out.println("Enter an x value for point 3: ");
 		xCoord = userInput.nextInt();
 		System.out.println("Enter a y value for point 3: ");
-		yCoord = userInput.nextInt();*/
-		Point point3 = new Point();
+		yCoord = userInput.nextInt();
+		Point point3 = new Point(xCoord,yCoord);
+		//prompt user for deltaX and deltaY values to use in the move method
+		int deltaX,deltaY;
+		System.out.println("Enter a value for deltaX: ");
+		deltaX = userInput.nextInt();
+		System.out.println("Enter a value for deltaY: ");
+		deltaY = userInput.nextInt();
+		//display point 1,2, and 3 coordinates
+		System.out.println("Point 1: " + "ID#  " + point1.ID + "\t" + point1);
+		System.out.println("Point 2: " + "ID#  " + point2.ID + "\t" + point2);
+		System.out.println("Point 3: " + "ID#  " + point3.ID + "\t" + point3);
+		//move points 1,2,and 3 by the values entered in for deltaX and deltaY
+		point1.move(deltaX,deltaY);
+		System.out.println("Point 1 moved to: " + point1);
+		point2.move(deltaX,deltaY);
+		System.out.println("Point 2 moved to: " + point2);
+		point3.move(deltaX,deltaY);
+		System.out.println("Point 3 moved to: " + point3);
 
-		point1.move(2,2);
-		System.out.println("move point:" + point1);
-		Point.distance(point1,point2);
-		//System.out.println("distance: " distance);
-		System.out.println("Point 1: " + point1);
-		
-		System.out.println("Point 2: " + point2);
+		System.out.println("Current number of active points: " + Point.activeIns);		
+		//instantiating a new object
+		Point point4 = new Point();
+		System.out.println("Point 4: ID# " + point4.ID + "\t" + point4);
+		System.out.printf("Distance between points 1 and 4: %.2f %n", point4.distance(point1));
+		System.out.printf("Distance between points 2 and 4: %.2f %n",point4.distance(point2));
+		System.out.printf("Distance between points 3 and 4: %.2f %n",Point.distance(point4,point3));
+		//instantiating a new object with coordinates that are the average of the first 3 points
+		Point point5 = new Point();
+		point5.average(point1,point2,point3);
+		System.out.println("Point 5: ID# " + point5.ID + "\t" + point5);
 
-		System.out.println("Point 3: " + point3);
-		System.out.println("Point 1 ID: " + point1.ID);
-		System.out.println("Point 2 ID: " + point2.ID);
-		System.out.println("Point 3 ID: " + point3.ID);
-		System.out.println("Current number of points: " + Point.activeIns);
+		System.out.println("Current number of active points: " + Point.activeIns);
 
 		point1 = null;
+		point2 = null;
+		point3 = null;
 		
 		Point.fullGC();
-		System.out.println("Current number of points: " + Point.activeIns);
 
-		Point point4 = new Point(3,7);
-		System.out.println("Point 4 ID: " + point4.ID);
 		System.out.println("Current number of points: " + Point.activeIns);
-
 		
 
 
